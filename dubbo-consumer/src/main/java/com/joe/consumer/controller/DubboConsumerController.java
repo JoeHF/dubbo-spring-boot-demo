412d0049ffb29bc00c7e1092c9d8f179d45030e4
joe.hou <joe.hou@airbnb.com>
2018-10-16 17:46:44 +0800

    update

--- dubbo-consumer/src/main/java/com/joe/consumer/controller/DubboConsumerController.java
+++ dubbo-consumer/src/main/java/com/joe/consumer/controller/DubboConsumerController.java
@@ -62,39 +62,4 @@ public class DubboConsumerController {
   public String theHandler() {
     return myBean.getMessage();
   }
-
-  @Autowired private PersonMapper personMapper;
-
-  @RequestMapping("/save")
-  public Integer save() {
-    PersonDO personDO = new PersonDO();
-    personDO.setName("张三");
-    personDO.setAge(18);
-    personMapper.insert(personDO);
-    return personDO.getId();
-  }
-
-  @RequestMapping("/update")
-  public Long update() {
-    PersonDO personDO = new PersonDO();
-    personDO.setId(2);
-    personDO.setName("旺旺");
-    personDO.setAge(12);
-    return personMapper.update(personDO);
-  }
-
-  @RequestMapping("/delete")
-  public Long delete() {
-    return personMapper.delete(11L);
-  }
-
-  @RequestMapping("/selectById")
-  public PersonDO selectById() {
-    return personMapper.selectById(2L);
-  }
-
-  @RequestMapping("/selectAll")
-  public List<PersonDO> selectAll() {
-    return personMapper.selectAll();
-  }
 }
